b1eeb0d51797cf8f2e80ddcb992f9ce7557fe793
lihip <lihipana@gmail.com>
2021-07-10 09:10:47 +0300

    seperate route, create of the api. get request with JSON

--- api/route.py
+++ api/route.py
@@ -1,34 +1,36 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import request, Blueprint
 
 from service import *
 
-app = Flask(__name__)
+main = Blueprint('main', __name__)
 
 
-@app.route("/")
-@app.route("/home", methods=['GET', 'POST'])
+@main.route("/")
+@main.route("/home", methods=['GET', 'POST'])
 def land():
     """Landing page"""
-    pass
+    return '<h1>"hello world"</h1>'
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@main.route("/login", methods=['GET', 'POST'])
 def login():
     """Log in page"""
-    email = request.form.get('email')
-    password = request.form.get('password')
+    account_data = request.get_json()
+    email = account_data['email']
+    password = account_data['password']
     validation = valid_input(email, password)
     if validation:
         return validation
     return valid_login(email, password)
 
 
-@app.route("/signup", methods=['GET', 'POST'])
+@main.route("/signup", methods=['GET', 'POST'])
 def signup():
-    """Sign in page"""
-    email = request.form.get('email')
-    username = request.form.get('username')
-    password = request.form.get('password')
+    """Sign up page"""
+    account_data = request.get_json()
+    email = account_data['email']
+    username = account_data['username']
+    password = account_data['password']
     validation = valid_input(email, username, password)
     if validation:
         return validation
@@ -40,11 +42,12 @@ def signup():
     return valid_signup(details)
 
 
-@app.route("/add_new_tree", methods=['GET', 'POST'])
+@main.route("/add_new_tree", methods=['GET', 'POST'])
 def add_new_tree():
     """add new person page"""
-    tree_name = request.form.get('tree_name')
-    token = request.form.get('token')
+    tree_data = request.get_json()
+    tree_name = tree_data['tree_name']
+    token = tree_data['token']
     validation = valid_input(tree_name, token)
     if validation:
         return validation
@@ -54,15 +57,16 @@ def add_new_tree():
     return add_tree(tree_name, token)
 
 
-@app.route("/add_new_person", methods=['GET', 'POST'])
+@main.route("/add_new_person", methods=['GET', 'POST'])
 def add_new_person():
     """add new person page"""
-    person_id = request.form.get('person_id')
-    first_name = request.form.get('first_name')
-    last_name = request.form.get('last_name')
-    gender = request.form.get('gender')
-    tree_id = request.form.get('tree_id')
-    token = request.form.get('token')
+    person_data = request.get_json()
+    person_id = person_data['person_id']
+    first_name = person_data['first_name']
+    last_name = person_data['last_name']
+    gender = person_data['gender']
+    tree_id = person_data['tree_id']
+    token = person_data['token']
     validation = valid_input(person_id, first_name, last_name, gender, tree_id, token)
     if validation:
         return validation
@@ -80,13 +84,14 @@ def add_new_person():
     return add_person(details)
 
 
-@app.route("/add_new_relation", methods=['GET', 'POST'])
+@main.route("/add_new_relation", methods=['GET', 'POST'])
 def add_new_relation():
     """add new person page. mother_id and father_id: at least one of the two exist"""
-    person_id = request.form.get('person_id')
-    mother_id = request.form.get('mother_id')
-    father_id = request.form.get('father_id')
-    token = request.form.get('token')
+    relation_data = request.get_json()
+    person_id = relation_data['person_id']
+    mother_id = relation_data['mother_id']
+    father_id = relation_data['father_id']
+    token = relation_data['token']
     validation = valid_input(person_id, mother_id, father_id, token)
     if validation:
         return validation
@@ -101,11 +106,12 @@ def add_new_relation():
     return add_relation(details)
 
 
-@app.route("/get_tree_table", methods=['GET', 'POST'])
+@main.route("/get_tree_table", methods=['GET', 'POST'])
 def get_table():
     """get table information"""
-    token = request.form['token']
-    name = request.form['tree_name']
+    tree_data = request.get_json()
+    token = tree_data['token']
+    name = tree_data['tree_name']
     validation = valid_input(token, name)
     if validation:
         return validation
@@ -113,7 +119,3 @@ def get_table():
     if token_validation:
         return token_validation
     return get_tree_information(token, name)
-
-
-if __name__ == "__main__":
-    app.run()
