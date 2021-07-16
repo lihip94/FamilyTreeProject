743d7e3b23fd3b50f59504b1b7d719df7a8d0f3c
lihip <lihipana@gmail.com>
2021-07-16 15:14:18 +0300

    update SQL command format

--- database/DB.py
+++ database/DB.py
@@ -86,77 +86,95 @@ class FamilyTreeDB:
         return account_data
 
     def email_exist(self, email_address):
-        self.cursor.execute("SELECT * FROM " + self.account.name + " WHERE email = %s", (email_address,))
+        select_command = "SELECT * FROM {} WHERE {} = %s"
+        self.cursor.execute(select_command.format(self.account.name, "email"), (email_address,))
         account_row_count = self.cursor.rowcount
         if account_row_count >= 1:
             return True
         return False
 
     def account_exist(self, email_address, password):
-        self.cursor.execute("SELECT * FROM " + self.account.name + " WHERE email = %s AND password = %s",
-                            (email_address, password))
+        select_command = "SELECT * FROM {} WHERE {} = %s AND {} = %s"
+        self.cursor.execute(select_command.format(self.account.name, "email", "password "), (email_address, password))
         account_row_count = self.cursor.rowcount
         if account_row_count >= 1:
             return True
         return False
 
     def person_exist(self, table_name, person_id):
-        self.cursor.execute("SELECT * FROM " + table_name + " WHERE id = %s", (person_id,))
+        select_command = "SELECT * FROM {} WHERE {} = %s"
+        self.cursor.execute(select_command.format(table_name, "id"), (person_id,))
         person_row_count = self.cursor.rowcount
         if person_row_count >= 1:
             return True
         return False
 
     def connection_exist(self, tree_id, email):
-        self.cursor.execute("SELECT * FROM connection WHERE tree_id = %s AND email = %s", (tree_id, email))
+        select_command = "SELECT * FROM {} WHERE {} = %s AND {} = %s"
+        self.cursor.execute(select_command.format(self.connection.name, "tree_id", "email"), (tree_id, email))
         connection_row_count = self.cursor.rowcount
         if connection_row_count > 0:
             return True
         return False
 
     def update_token(self, token, email_address):
-        self.cursor.execute("UPDATE " + self.account.name + " SET token = %s WHERE email = %s", (token, email_address))
+        update_command = "UPDATE {} SET {} = %s WHERE {} = %s"
+        update_command.format(self.account.name, "token", "email")
+        self.cursor.execute(update_command.format(self.account.name, "token", "email"), (token, email_address))
         self.db.commit()
 
     def token_exist(self, token):
-        self.cursor.execute("SELECT * FROM account WHERE token = %s", (token,))
+        select_command = "SELECT * FROM {} WHERE {} = %s"
+        self.cursor.execute(select_command.format(self.account.name, "token"), (token,))
         token_row_count = self.cursor.rowcount
         if token_row_count == 1:
             return True
         return False
 
     def tree_exist(self, tree_name):
-        self.cursor.execute("SELECT * FROM tree WHERE name = %s", (tree_name,))
+        select_command = "SELECT * FROM {} WHERE {} = %s"
+        self.cursor.execute(select_command.format(self.tree.name, "name"), (tree_name,))
         tree_row_count = self.cursor.rowcount
         if tree_row_count >= 1:
             return True
         return False
 
     def get_email(self, token):
-        self.cursor.execute("SELECT email FROM account WHERE token = %s", (token,))
-        return self.cursor.fetchone()
+        select_command = "SELECT {} FROM {} WHERE {} = %s"
+        self.cursor.execute(select_command.format("email", self.account.name, "token"), (token,))
+        email = self.cursor.fetchone()
+        if email:
+            return email[0]
+        return False
 
     def get_tree_id(self, tree_name):
-        self.cursor.execute("SELECT id FROM tree WHERE name = %s", (tree_name,))
+        select_command = "SELECT {} FROM {} WHERE {} = %s"
+        self.cursor.execute(select_command.format("id", self.tree.name, "name"), (tree_name,))
         tree_id = self.cursor.fetchone()
         if tree_id:
             return tree_id[0]
         return False
 
     def tree_not_connect_to_user(self, tree_id, token):
-        self.cursor.execute("SELECT tree_id FROM account,connection where account.token= %s"
-                            " AND account.email=connection.email AND connection.tree_id= %s", (token, tree_id))
+        select_command = "SELECT {} FROM {},{} WHERE {}= %s AND {}={} AND {}= %s"
+        account_token = self.account.name + ".token"
+        account_email = self.account.name + ".email"
+        connection_email = self.connection.name + ".email"
+        self.cursor.execute(
+            select_command.format("tree_id", self.account.name, self.connection.name, account_token,
+                                  account_email, connection_email, "tree_id"), (token, tree_id))
         tree_row_count = self.cursor.rowcount
         if tree_row_count < 1:
             return True
         return False
 
     def get_tree_persons(self, tree_id):
-        self.cursor.execute("SELECT person_id FROM root WHERE tree_id= %s", (tree_id,))
+        select_command = "SELECT {} FROM {} WHERE {} = %s"
+        self.cursor.execute(select_command.format("person_id", self.root.name, "tree_id"), (tree_id,))
         persons_id = self.cursor.fetchall()
         persons_in_tree = []
         for person_id in persons_id:
-            self.cursor.execute("SELECT * FROM person WHERE id= %s", (person_id[0],))
+            self.cursor.execute(select_command.format("*", self.person.name, "id"), (person_id[0],))
             person_data = self.cursor.fetchone()
             persons_in_tree.append(person_data)
         return persons_in_tree
